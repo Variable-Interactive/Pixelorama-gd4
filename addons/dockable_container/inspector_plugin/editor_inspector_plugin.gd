b8c473e40332d4e2aba661675cbb69b5c2f8ee9d
Variable <77773850+Variable-ind@users.noreply.github.com>
2023-03-07 22:23:48 +0500

    Add files via upload

--- addons/dockable_container/inspector_plugin/editor_inspector_plugin.gd
+++ addons/dockable_container/inspector_plugin/editor_inspector_plugin.gd
@@ -3,16 +3,12 @@ extends EditorInspectorPlugin
 const DockableContainer = preload("../dockable_container.gd")
 const LayoutEditorProperty = preload("layout_editor_property.gd")
 
-
-func can_handle(object: Object) -> bool:
+func _can_handle(object: Object) -> bool:
 	return object is DockableContainer
 
 
-func parse_property(
-	_object: Object, _type: int, path: String, _hint: int, _hint_text: String, _usage: int
-) -> bool:
+func _parse_property(object: Object, type: Variant.Type, path: String, hint: PropertyHint, hint_text: String, usage: PropertyUsageFlags, wide: bool) -> bool:
 	if path == "layout":
 		var editor_property = LayoutEditorProperty.new()
 		add_property_editor("layout", editor_property)
 	return false
-
